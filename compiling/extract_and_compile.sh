8f2c695962ceb53eed642491707c9c88c15abfdd
Cole Maxwell <maxwe206@morris.umn.edu>
2021-08-31 11:23:43 -0500

    add gcc comp for NthPrime to script

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,11 +2,17 @@
 
 nthArg=$1
 
-#Uncompress and extract NthPrime.tgz into current directory.
+#Uncompress and extract NthPrime.tgz into current directory
 #x - extracts contents of given file
 #f - name of file to uncompress
 #z - uncompress tarball via gzip
-tar -xfz NthPrime.tgz
+tar xfz NthPrime.tgz
 
 #cd into newly created NthPrime directory
 cd NthPrime
+
+#Compile NthPrime program using gcc (preprocesses, compiles, and assembles) 
+gcc *.c -o NthPrime
+
+#Call the NthPrime program and passes it the arguement passed when calling the extract_and_compile function
+NthPrime $nthArg
