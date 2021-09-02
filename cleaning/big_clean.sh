c40787ff5df1386f5cbe14b002d0e0b0c531cd4a
Cole Maxwell <maxwe206@morris.umn.edu>
2021-09-02 11:39:07 -0500

    Add method to delete files and re-tar

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 userInput=$1
+tempDir=$(mktemp -d)
+userDir=$(pwd)
 
-tar xzf "$userInput"
+#Uncompress and extract input file into temp directory
+# -C specifies the directory to put things into
+# -xzf specify to extract, unzip, and that the input file is an archive
+tar C $tempDir xzf $userInput
 
+#Search the files for those containing the given string
+# -R search recursively
+# -l return the files containing the string
+#| delete the given files
+# -f force it
+grep "DELETE ME!" $tempDir Rl | xargs rm f
 
+cd $tempDir || exit
+
+tar czf "cleaned_$userInput" $userInput
