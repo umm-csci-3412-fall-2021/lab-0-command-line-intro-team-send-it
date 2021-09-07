ce0e1d8ba8d6c3766744d834fae4854430db05cc
Cole Maxwell <cole.n.maxwell99@gmail.com>
2021-09-07 10:46:17 -0500

    Fix issues that shellcheck found

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -7,22 +7,22 @@ tempDir=$(mktemp -d)
 #Variable storing the user's working directory
 userDir=$(pwd)
 #Removes the file extention .tgz from userInput
-basedName=$(basename -s .tgz $userInput)
+basedName=$(basename -s .tgz "$userInput")
 
 #Uncompress and extract input file into temp directory
 # -C specifies the directory to put things into
 # -xzf specify to extract, unzip, and that the input file is an archive
-tar xzf $userInput -C $tempDir
+tar xzf "$userInput" -C "$tempDir"
 
 #Search the files for those containing the given string
 # -R search recursively
 # -l return the files containing the string
 #| delete the given files
 # -f force it
-grep "DELETE ME!" $tempDir -Rl | xargs rm -f
+grep "DELETE ME!" "$tempDir" -Rl | xargs rm -f
 
 #Change directory to the temp directory
-cd $tempDir
+cd "$tempDir" || exit
 
 #tar together and compressing the contents of the temp diectory
-tar czf $userDir/"cleaned_$userInput" $basedName --warning=no-file-changed
+tar czf "$userDir/cleaned_$userInput" "$basedName" --warning=no-file-changed
